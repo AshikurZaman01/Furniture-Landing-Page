5f0859b60be5695286cb224b225eea28d0d098c4
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-10-17 16:18:09 +0600

    add scroll effect in navbar

--- src/Components/Pages/Navbar/Navbar.jsx
+++ src/Components/Pages/Navbar/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
 import Logo from './Logo'
 import MenuItems from './MenuItems/MenuItems'
 import CartBtn from './CartBtn'
+import { useEffect, useState } from 'react'
 
 const Navbar = () => {
+
+    const [isScrolled, setIsScrolles] = useState(false);
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolles(true)
+            } else {
+                setIsScrolles(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+
+    }, [])
+
     return (
-        <div className='fixed top-0 left-0 right-0 z-50 transition duration-300 ease-in-out text-white'>
+        <div className={`fixed top-0 left-0 right-0 z-50 transition duration-300 ease-in-out  ${isScrolled ? 'bg-white shadow-md ' : 'bg-transparent text-white'}`}>
             <nav className='max-w-screen-xl container  mx-auto flex justify-between items-center  px-4'>
 
                 <Logo></Logo>
